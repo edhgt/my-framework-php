1112bdf4be2d00bef1e9c5aa111f5bb44e1da284
Danilo Hernandez <dhernandez0032@gmail.com>
2021-05-27 18:52:12 -0600

    response

---
+++ app/Http/Response.php
@@ -0,0 +1,30 @@
+<?php 
+
+namespace App\Http;
+
+/**
+ * Response $response
+ */
+class Response
+{
+	protected $view; //array, json, pdf...
+
+	public function __construct($view)
+	{
+		$this->view = $view;
+	}
+
+	public function getView()
+	{
+		return $this->view;
+	}	
+
+	public function send()
+	{
+		$view = $this->getView();
+
+		$content = file_get_contents(__DIR__ . "/../../views/$view.php");
+
+		require __DIR__ . "/../../views/layout.php";
+	}
+}
